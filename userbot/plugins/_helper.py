7d11b404cec3648e23e8e506bce07f7bdeb184b9
pOpzZ <58160780+yu9ohde@users.noreply.github.com>
2020-10-23 22:50:15 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -9,7 +9,7 @@ async def cmd_list(event):
         if tgbotusername is None or input_str == "text":
             string = ""
             for i in CMD_LIST:
-                string += "ℹ️ " + i + "\n"
+                string += "🙄 " + i + "\n"
                 for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
@@ -29,8 +29,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Friday Userbot Modules Are Listed Here !\n
-For More Help or Support Visit @FridayOT"""
+            help_string = """Userbot Modules Are...... """
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
             )
